d241dd53eb9ca964420c5d1dfba8072ea33f47c5
Patrick Lebsack <p_lebbysack@hotmail.com>
2019-02-11 22:18:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
   return new_array
 }
 
-function destructivelyAddElementToBeginningOfArray (array, elemen){
+function destructivelyAddElementToBeginningOfArray (array, element){
   array.unshift(element)
   return array
 }
