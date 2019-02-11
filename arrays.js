4796e9a2c2ae27a5e342a98e5a4e06533ee04316
Patrick Lebsack <p_lebbysack@hotmail.com>
2019-02-11 22:07:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var new_array = [element, array]
+  var new_array = [element, array(0)]
 }
