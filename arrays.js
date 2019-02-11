0c6863423b4bb29d1eadb0660bfe44620298482b
Patrick Lebsack <p_lebbysack@hotmail.com>
2019-02-11 22:57:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,5 +31,10 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
 
   
